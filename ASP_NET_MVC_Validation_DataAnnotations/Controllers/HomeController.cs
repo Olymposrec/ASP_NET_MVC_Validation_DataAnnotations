e47f6a9587289e57c80965197e7cb7e7382b0dca
Melih <Olymposrec@users.noreply.github.com>
2020-08-05 18:05:57 +0300

    ASP_NET_MVC_Validation_DataAnnotations
    ASP.NET MVC' nin bu bölümünde validation ve DataAnnotations' a bakacağız.

---
+++ ASP_NET_MVC_Validation_DataAnnotations/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using ASP_NET_MVC_Validation_DataAnnotations.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASP_NET_MVC_Validation_DataAnnotations.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View(new Users());
+        }
+        [HttpPost]
+        public ActionResult Index(Users model)
+        {
+            //ModelState.IsValid
+            if (model.kullaniciadi == "melihakkose")
+            {
+                //ModelState.AddModelError("KullaniciAdi(key)", "Bu kullanıcı adı kullanılıyor.") Validation Summary' de çıkmaz
+                ModelState.AddModelError("", "Bu kullanıcı adı kullanılıyor."); /*Validation Summary' de çıkar*/
+            }
+            return View(model);
+        }
+    }
+}
---
+++ ASP_NET_MVC_Validation_DataAnnotations/Models/Users.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ASP_NET_MVC_Validation_DataAnnotations.Models
+{
+    public class Users
+    {
+        [DisplayName("Adınız")]
+        public string ad { get; set; }
+
+        [DisplayName("Soyadınız")]
+        public string soyad { get; set; }
+
+        [DisplayName("Kullanıcı Adınız"), 
+            Required(ErrorMessage ="Bu Alan Boş Geçilemez!"), 
+            MinLength(8,ErrorMessage ="Minimum {1} Karakter olmalı."), 
+            MaxLength(25, ErrorMessage = "Maksimum {1} karakter olmalı.")]
+        public string kullaniciadi { get; set; }
+
+        [DisplayName("Şifre"), 
+            Required(ErrorMessage = "Bu Alan Boş Geçilemez!"), 
+            MinLength(12, ErrorMessage = "Minimum {1} Karakter olmalı."), 
+            MaxLength(25, ErrorMessage = "Maksimum {1} karakter olmalı."), 
+            DataType(DataType.Password)]
+        public string sifre { get; set; }
+
+        [DisplayName("Şifre Onay"), 
+            Required(ErrorMessage = "Bu Alan Boş Geçilemez!"), 
+            MinLength(12, ErrorMessage = "Minimum {1} Karakter olmalı."), 
+            MaxLength(25, ErrorMessage = "Maksimum {1} karakter olmalı."),
+            DataType(DataType.Password), 
+            Compare(nameof(sifre),ErrorMessage ="Şifreler Eşeleşmiyor!")]
+        public string sifre2 { get; set; }
+
+        [DisplayName("E-Posta Adresi"), 
+            Required(ErrorMessage = "Bu Alan Boş Geçilemez!"), 
+            MinLength(12, ErrorMessage = "Minimum {1} Karakter olmalı."), 
+            MaxLength(35, ErrorMessage = "Maksimum {1} karakter olmalı."),
+            EmailAddress()]
+        public string Eposta { get; set; }
+
+        [DisplayName("E-Posta Adresi Onay"), 
+            Required(ErrorMessage = "Bu Alan Boş Geçilemez!"), 
+            MinLength(12, ErrorMessage = "Minimum {1} Karakter olmalı."), 
+            MaxLength(35,ErrorMessage = "Maksimum {1} karakter olmalı."), 
+            DataType(DataType.EmailAddress), 
+            Compare(nameof(Eposta), ErrorMessage = "Mailler Eşeleşmiyor!")]
+        public string Eposta2 { get; set; }
+
+        
+
+
+    }
+}
